de01f5e71b8c729c19591d41a841531c882bccc2
Nebojsa-Sladojevic <nebojsa.sladojevic1@gmail.com>
2025-11-24 02:22:08 +0100

    v0.16-beta - Add LoginWindow and Paperless-NGX logo

---
+++ Client-Application/Windows/SignInWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ClientApplication.Windows
+{
+    public partial class SignInWindow : Window
+    {
+        public SignInWindow() => InitializeComponent();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MinWidth = ActualWidth;
+            MinHeight = ActualHeight;
+        }
+    }
+}
